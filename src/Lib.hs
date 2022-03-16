9866d16f1c45e8bef185dab86b50ded71939e41a
Evgeniy Rybin <z.ribin20@gmail.com>
2022-03-16 14:57:56 +0500

    Add longpoll methods for TG

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,13 +13,18 @@ where
 
 import qualified Control.Exception as E
 import qualified Control.Monad.Catch as MCatch
-import Control.Monad.Reader (MonadIO (..), MonadReader (ask), ReaderT (runReaderT))
+import Control.Monad.Reader (MonadIO (..), MonadReader (ask), ReaderT (runReaderT), guard)
+import Data.Aeson ((.:))
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as AT
 import Data.ByteString.Lazy (ByteString)
 import Data.Typeable (Typeable)
+import qualified Data.Vector as V
 import qualified Network.HTTP.Base as HBase
 import qualified Network.HTTP.Client as HClient
 import qualified Network.HTTP.Client.TLS as TLS
 import qualified Network.HTTP.Types.Status as HStatus
+import qualified TelegramTypes as TGTypes
 import Text.Printf (printf)
 
 startTG = do
@@ -91,6 +96,16 @@ data Bot
       { botToken :: String
       }
 
+data LongPoll
+  = VKLongpoll
+      { lpServer :: String,
+        lpKey :: String,
+        lpTS :: String
+      }
+  | TelegramLongpoll
+      { lpLastUpdateID :: Integer
+      }
+
 data APIException = APIException
   deriving (Show, Typeable)
 
@@ -144,3 +159,68 @@ sendAPIMethod method params = do
     code -> do
       logMessage Error $ printf "Status %d %s" code (show $ HStatus.statusMessage status)
       E.throw APIException
+
+initLongpoll ::
+  ( MonadReader env m,
+    MonadIO m,
+    MCatch.MonadThrow m,
+    HasBot env,
+    HasLog env,
+    HasManager env
+  ) =>
+  m (TGTypes.Updates, LongPoll)
+initLongpoll = do
+  env <- ask
+  case getBot env of
+    bot@TelegramBot {} -> do
+      resp <- sendAPIMethod "getUpdates" []
+      let updates = A.decode resp :: Maybe TGTypes.Updates
+      case updates of
+        Just udts ->
+          if TGTypes.usOk udts
+            then do
+              let newUpdateID = TGTypes.uID . last . TGTypes.usUpdates $ udts
+              return (udts, TelegramLongpoll {lpLastUpdateID = newUpdateID})
+            else undefined
+        Nothing -> undefined
+    bot@VKBot {} -> undefined
+
+awaitLongpoll ::
+  ( MonadReader env m,
+    MonadIO m,
+    MCatch.MonadThrow m,
+    HasBot env,
+    HasLog env,
+    HasManager env
+  ) =>
+  LongPoll ->
+  m (TGTypes.Updates, LongPoll)
+awaitLongpoll longpoll@TelegramLongpoll {lpLastUpdateID = updateID} = do
+  resp <- sendAPIMethod "getUpdates" [("timeout", "25"), ("offset", show $ updateID + 1)]
+  logMessage Debug $ show resp
+  let updates = A.decode resp :: Maybe TGTypes.Updates
+  case updates of
+    Just udts ->
+      if TGTypes.usOk udts
+        then do
+          let newUpdateID = TGTypes.uID . last . TGTypes.usUpdates $ udts
+          return (udts, longpoll {lpLastUpdateID = newUpdateID})
+        else undefined
+    Nothing -> undefined
+awaitLongpoll VKLongpoll {} = undefined
+
+handleLongpoll ::
+  ( MonadReader env m,
+    MonadIO m,
+    MCatch.MonadThrow m,
+    HasBot env,
+    HasLog env,
+    HasManager env
+  ) =>
+  LongPoll ->
+  (TGTypes.Updates -> m ()) ->
+  m ()
+handleLongpoll longpoll handler = do
+  (updates, lp) <- awaitLongpoll longpoll
+  handler updates
+  handleLongpoll lp handler
---
+++ src/TelegramTypes.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TelegramTypes (Updates (..), Update (..), Message (..), User (..)) where
+
+import Control.Applicative (Alternative (empty))
+import Data.Aeson (FromJSON (parseJSON), Value (Object), (.:))
+
+data Updates = Updates
+  { usOk :: Bool,
+    usUpdates :: [Update]
+  } deriving (Show)
+
+instance FromJSON Updates where
+  parseJSON (Object updates) = Updates <$> updates .: "ok" <*> updates .: "result"
+  parseJSON _ = empty
+
+data Update = Update
+  { uID :: Integer,
+    uMessage :: Message
+  }
+  deriving (Show)
+
+instance FromJSON Update where
+  parseJSON (Object update) = Update <$> update .: "update_id" <*> update .: "message"
+  parseJSON _ = empty
+
+data Message = Message
+  { mFrom :: User
+  }
+  deriving (Show)
+
+instance FromJSON Message where
+  parseJSON (Object message) = Message <$> message .: "from"
+  parseJSON _ = empty
+
+data User = User
+  { userID :: Integer
+  }
+  deriving (Show)
+
+instance FromJSON User where
+  parseJSON (Object user) = User <$> user .: "id"
+  parseJSON _ = empty
